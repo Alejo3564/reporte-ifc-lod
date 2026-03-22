f9c795ae6bac4d5952a25a4ce8139588604179c7
Alejo3564 <159055918+Alejo3564@users.noreply.github.com>
2026-03-21 20:42:26 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,24 @@
-import resolve from "@rollup/plugin-node-resolve";
+import resolve  from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import copy     from "rollup-plugin-copy";
 
 export default {
   input: "src/main.js",
   output: {
-    file: "dist/bundle.js",
+    file:   "dist/bundle.js",
     format: "iife",
-    name: "App",
+    name:   "App",
     inlineDynamicImports: true,
   },
   plugins: [
-    resolve({ browser: true }),
+    resolve({ browser: true, preferBuiltins: false }),
     commonjs(),
+    copy({
+      targets: [
+        // WASM de web-ifc (requerido por @thatopen)
+        { src: "node_modules/web-ifc/*.wasm", dest: "dist" },
+      ],
+      hook: "writeBundle",
+    }),
   ],
 };
